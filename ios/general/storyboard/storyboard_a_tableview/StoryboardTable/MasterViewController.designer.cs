23baf606d4ef992f6d3c0a457d628e31d8574539
Amy Burns <amy.burns87@gmail.com>
2015-07-16 19:37:37 -0400

    [STORYBOARDS]Update for unified api

--- ios/general/storyboard/storyboard_a_tableview/StoryboardTable/MasterViewController.designer.cs
+++ ios/general/storyboard/storyboard_a_tableview/StoryboardTable/MasterViewController.designer.cs
@@ -4,10 +4,10 @@
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
+using Foundation;
 using System;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
+using UIKit;
 
 namespace StoryboardTable
 {
