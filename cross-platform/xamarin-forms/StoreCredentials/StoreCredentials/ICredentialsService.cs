5400b0ea4a0ac8da7c3e985844aa8ae25af1305d
David Britch <davidbritch@Davids-MacBook-Pro.local>
2015-10-19 12:48:09 +0100

    Storing credentials recipe added.

---
+++ cross-platform/xamarin-forms/StoreCredentials/StoreCredentials/ICredentialsService.cs
@@ -0,0 +1,15 @@
+﻿namespace StoreCredentials
+{
+	public interface ICredentialsService
+	{
+		string UserName { get; }
+
+		string Password { get; }
+
+		void SaveCredentials (string userName, string password);
+
+		void DeleteCredentials ();
+
+		bool DoCredentialsExist ();
+	}
+}
