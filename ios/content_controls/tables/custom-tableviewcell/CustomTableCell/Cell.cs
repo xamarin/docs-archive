9cf6e6b032563f831ea940c177477afefa08016d
Amy Burns <amy.burns87@gmail.com>
2015-11-10 19:37:59 -0500

    [CustomTableViewCell]Add recipe

---
+++ ios/content_controls/tables/custom-tableviewcell/CustomTableCell/Cell.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace CustomTableCell
+{
+	partial class Cell : UITableViewCell
+	{
+		public Cell (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
