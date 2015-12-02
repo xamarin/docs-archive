bd2ae3b2f9543bdbd4140f09c6dcd0ce98bdf285
lobrien <lobrien@knowing.net>
2015-12-01 17:18:48 -1000

    Hello World Urho in F#

---
+++ cross-platform/game_development/urho-fsharp/HelloWorldUrhoFsharp/HelloWorldUrho1/AppDelegate.fs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorldUrho1
+
+open System
+open UIKit
+open Foundation
+open Urho
+open Urho.iOS
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
+
+    //override val Window = null with get, set
+
+    override this.FinishedLaunching (app, options) =
+        //this.Window <- new UIWindow(UIScreen.MainScreen.Bounds)
+
+        UrhoEngine.Init()
+        let o = Urho.ApplicationOptions.Default
+     
+        let g = new HelloWorld(o)
+        g.Run() |> ignore
+       
+        true
