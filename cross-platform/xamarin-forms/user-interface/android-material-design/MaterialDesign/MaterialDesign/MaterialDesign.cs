fb55cb8a2f541b03db5c614268216654128f395a
Nathan C. Castle <nathan.castle@outlook.com>
2015-12-03 12:11:08 -0800

    Added recipe for xamarin.forms material design

---
+++ cross-platform/xamarin-forms/user-interface/android-material-design/MaterialDesign/MaterialDesign/MaterialDesign.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MaterialDesign
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!"
+						},
+						new Button {Text="Click Me"},
+						new Slider {Value = .5}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ cross-platform/xamarin-forms/user-interface/android-material-design/README.md
@@ -0,0 +1,3 @@
+This recipe shows how to add Material Design to your Xamarin.Forms Android projects.
+
+The full recipe can be found [here](http://developer.xamarin.com/recipes/cross-platform/xamarin-forms/user-interface/android-material-design/).
