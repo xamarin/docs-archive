9489e6902323882b16a118512ac4821a16d75c81
Tom Opgenorth <tom@opgenorth.net>
2016-01-13 12:45:51 -0700

    Code formatting.

--- ios/media/video_and_photos/choose_a_photo_from_the_gallery/ImageViewController.cs
+++ ios/media/video_and_photos/choose_a_photo_from_the_gallery/ImageViewController.cs
@@ -4,35 +4,37 @@ using AssetsLibrary;
 using UIKit;
 using Foundation;
 
-namespace ImageView {
+namespace ImageView
+{
 
-	public class ImageViewController : UIViewController {
+	public class ImageViewController : UIViewController
+	{
 		
 		UIImagePickerController imagePicker;
 		UIButton choosePhotoButton;
 		UIImageView imageView;
 
-		public override void ViewDidLoad ()
+		public override void ViewDidLoad()
 		{
-			base.ViewDidLoad ();
+			base.ViewDidLoad();
 			Title = "Choose Photo";
 			View.BackgroundColor = UIColor.White;
 
 			imageView = new UIImageView(new CGRect(10, 150, 300, 300));
-			Add (imageView);
+			Add(imageView);
 
-			choosePhotoButton = UIButton.FromType (UIButtonType.RoundedRect);
-			choosePhotoButton.Frame = new CGRect(10, 80, 100,40);
-			choosePhotoButton.SetTitle ("Picker", UIControlState.Normal);
+			choosePhotoButton = UIButton.FromType(UIButtonType.RoundedRect);
+			choosePhotoButton.Frame = new CGRect(10, 80, 100, 40);
+			choosePhotoButton.SetTitle("Picker", UIControlState.Normal);
 			choosePhotoButton.TouchUpInside += (s, e) => {
 				// create a new picker controller
-				imagePicker = new UIImagePickerController ();
+				imagePicker = new UIImagePickerController();
 				
 				// set our source to the photo library
 				imagePicker.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
 								
 				// set what media types
-				imagePicker.MediaTypes = UIImagePickerController.AvailableMediaTypes (UIImagePickerControllerSourceType.PhotoLibrary);
+				imagePicker.MediaTypes = UIImagePickerController.AvailableMediaTypes(UIImagePickerControllerSourceType.PhotoLibrary);
 				
 				imagePicker.FinishedPickingMedia += Handle_FinishedPickingMedia;
 				imagePicker.Canceled += Handle_Canceled;
@@ -42,17 +44,18 @@ namespace ImageView {
 				//UIPopoverController picc = new UIPopoverController(imagePicker);
 
 			};
-			View.Add (choosePhotoButton);
+			View.Add(choosePhotoButton);
 		}
 		
-		// Do something when the 
-		void Handle_Canceled (object sender, EventArgs e) {
-			Console.WriteLine ("picker cancelled");
+		// Do something when the
+		void Handle_Canceled(object sender, EventArgs e)
+		{
+			Console.WriteLine("picker cancelled");
 			imagePicker.DismissModalViewController(true);
 		}
 
 		// This is a sample method that handles the FinishedPickingMediaEvent
-		protected void Handle_FinishedPickingMedia (object sender, UIImagePickerMediaPickedEventArgs e)
+		protected void Handle_FinishedPickingMedia(object sender, UIImagePickerMediaPickedEventArgs e)
 		{
 			// determine what was selected, video or image
 			bool isImage = false;
@@ -72,48 +75,54 @@ namespace ImageView {
 			
 			// get common info (shared between images and video)
 			NSUrl referenceURL = e.Info[new NSString("UIImagePickerControllerReferenceUrl")] as NSUrl;
-			if (referenceURL != null) 
-				Console.WriteLine(referenceURL.ToString ());
+			if(referenceURL != null)
+				Console.WriteLine(referenceURL.ToString());
 			
 			// if it was an image, get the other image info
-			if(isImage) {
+			if(isImage)
+			{
 				
 				// get the original image
 				UIImage originalImage = e.Info[UIImagePickerController.OriginalImage] as UIImage;
-				if(originalImage != null) {
+				if(originalImage != null)
+				{
 					// do something with the image
-					Console.WriteLine ("got the original image");
-imageView.Image = originalImage;
+					Console.WriteLine("got the original image");
+					imageView.Image = originalImage;
 				}
 				
 				// get the edited image
 				UIImage editedImage = e.Info[UIImagePickerController.EditedImage] as UIImage;
-				if(editedImage != null) {
+				if(editedImage != null)
+				{
 					// do something with the image
-					Console.WriteLine ("got the edited image");
-imageView.Image = editedImage;
+					Console.WriteLine("got the edited image");
+					imageView.Image = editedImage;
 				}
 				
 				//- get the image metadata
 				NSDictionary imageMetadata = e.Info[UIImagePickerController.MediaMetadata] as NSDictionary;
-				if(imageMetadata != null) {
+				if(imageMetadata != null)
+				{
 					// do something with the metadata
-					Console.WriteLine ("got image metadata");
+					Console.WriteLine("got image metadata");
 				}
 				
 			}
 			// if it's a video
-			else {
+			else
+			{
 				// get video url
 				NSUrl mediaURL = e.Info[UIImagePickerController.MediaURL] as NSUrl;
-				if(mediaURL != null) {
+				if(mediaURL != null)
+				{
 					//
 					Console.WriteLine(mediaURL.ToString());
 				}
 			}
 			
 			// dismiss the picker
-			imagePicker.DismissModalViewController (true);
+			imagePicker.DismissModalViewController(true);
 		}
 	}
 }
