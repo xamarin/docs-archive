8926f774cfc91109133d5bdd16743a3a755bfb5b
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-01-18 15:11:06 +0000

    [DisplayPDF] Switched iOS custom renderer to a ViewRenderer in order to fix the display bug. Fixes #8

--- cross-platform/xamarin-forms/Controls/DisplayPDF/iOS/CustomWebViewRenderer.cs
+++ cross-platform/xamarin-forms/Controls/DisplayPDF/iOS/CustomWebViewRenderer.cs
@@ -2,23 +2,30 @@
 using DisplayPDF;
 using DisplayPDF.iOS;
 using Foundation;
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
 [assembly: ExportRenderer (typeof(CustomWebView), typeof(CustomWebViewRenderer))]
 namespace DisplayPDF.iOS
 {
-	public class CustomWebViewRenderer : WebViewRenderer
+	public class CustomWebViewRenderer : ViewRenderer<CustomWebView, UIWebView>
 	{
-		protected override void OnElementChanged (VisualElementChangedEventArgs e)
+		protected override void OnElementChanged (ElementChangedEventArgs<CustomWebView> e)
 		{
 			base.OnElementChanged (e);
 
+			if (Control == null) {
+				SetNativeControl (new UIWebView ());
+			}
+			if (e.OldElement != null) {
+				// Cleanup
+			}
 			if (e.NewElement != null) {
 				var customWebView = Element as CustomWebView;
 				string fileName = Path.Combine (NSBundle.MainBundle.BundlePath, string.Format ("Content/{0}", customWebView.Uri));
-				LoadRequest (new NSUrlRequest (new NSUrl (fileName, false)));
-				ScalesPageToFit = true;
+				Control.LoadRequest (new NSUrlRequest (new NSUrl (fileName, false)));
+				Control.ScalesPageToFit = true;
 			}
 		}
 	}
