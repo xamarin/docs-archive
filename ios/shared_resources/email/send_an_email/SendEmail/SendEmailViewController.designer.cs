6399a13c3ea8d64ca986f2dd915b707e1e33b7c6
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-04-10 16:50:09 -0700

    [ios] email code tweak for bugzilla 11702

--- ios/shared_resources/email/send_an_email/SendEmail/SendEmailViewController.designer.cs
+++ ios/shared_resources/email/send_an_email/SendEmail/SendEmailViewController.designer.cs
@@ -1,17 +1,29 @@
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace SendEmail
 {
-    [Register ("SendEmailViewController")]
-    partial class SendEmailViewController
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("SendEmailViewController")]
+	partial class SendEmailViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton sendButton { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (sendButton != null) {
+				sendButton.Dispose ();
+				sendButton = null;
+			}
+		}
+	}
 }
