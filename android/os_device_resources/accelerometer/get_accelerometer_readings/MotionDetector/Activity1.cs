d38fc8894f524a0f009e43d99697f990acad3445
Tom Opgenorth <tom@opgenorth.net>
2017-01-13 09:40:27 -0700

    Sync the variable names with the recipe.

--- android/os_device_resources/accelerometer/get_accelerometer_readings/MotionDetector/Activity1.cs
+++ android/os_device_resources/accelerometer/get_accelerometer_readings/MotionDetector/Activity1.cs
@@ -8,9 +8,9 @@ namespace MotionDetector
     [Activity(Label = "MotionDetector", MainLauncher = true, Icon = "@drawable/icon")]
     public class Activity1 : Activity, ISensorEventListener
     {
-		static readonly object syncLock = new object();
-		SensorManager sensorManager;
-		TextView sensorTextView;
+		static readonly object _syncLock = new object();
+		SensorManager _sensorManager;
+		TextView _sensorTextView;
 
         public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
         {
@@ -19,9 +19,9 @@ namespace MotionDetector
 
         public void OnSensorChanged(SensorEvent e)
         {
-            lock (syncLock)
+            lock (_syncLock)
             {
-                sensorTextView.Text = string.Format("x={0:f}, y={1:f}, z={2:f}", e.Values[0], e.Values[1], e.Values[2]);
+                _sensorTextView.Text = string.Format("x={0:f}, y={1:f}, z={2:f}", e.Values[0], e.Values[1], e.Values[2]);
             }
         }
 
@@ -29,22 +29,22 @@ namespace MotionDetector
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
-            sensorManager = (SensorManager) GetSystemService(SensorService);
-            sensorTextView = FindViewById<TextView>(Resource.Id.accelerometer_text);
+            _sensorManager = (SensorManager) GetSystemService(SensorService);
+            _sensorTextView = FindViewById<TextView>(Resource.Id.accelerometer_text);
         }
 
         protected override void OnResume()
         {
             base.OnResume();
-            sensorManager.RegisterListener(this,
-                                            sensorManager.GetDefaultSensor(SensorType.Accelerometer),
+            _sensorManager.RegisterListener(this,
+                                            _sensorManager.GetDefaultSensor(SensorType.Accelerometer),
                                             SensorDelay.Ui);
         }
 
         protected override void OnPause()
         {
             base.OnPause();
-            sensorManager.UnregisterListener(this);
+            _sensorManager.UnregisterListener(this);
         }
     }
 }
