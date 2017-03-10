5d617b22fb87760f0e39fafe3a38f2061b5d79f0
Kevin Mullins <kevin.mullins@xamarin.com>
2017-03-10 09:33:41 -0600

    iOS: Load an Image
    Updated the sample recipe project for Load an Image to bring it
    up-to-date and make it match the new Recipe text.

--- ios/standard_controls/image_view/load_an_image/Main.cs
+++ ios/standard_controls/image_view/load_an_image/Main.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using UIKit;
 
-using Foundation;
-using UIKit;
-
-namespace MapView {
-	public class Application {
+namespace ImageView
+{
+	public class Application
+	{
 		// This is the main entry point of the application.
-		static void Main (string[] args)
+		static void Main (string [] args)
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
---
+++ ios/standard_controls/image_view/load_an_image/ViewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using UIKit;
+
+namespace ImageView
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Create new image view and display it
+			var imageView = new UIImageView (UIImage.FromBundle ("MonkeySFO"));
+			imageView.Frame = new CoreGraphics.CGRect (10, 32, imageView.Image.CGImage.Width, imageView.Image.CGImage.Height);
+			View.AddSubview(imageView);
+
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
