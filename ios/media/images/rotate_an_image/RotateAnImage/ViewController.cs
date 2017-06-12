63aa760c592a57f19b837e742e7497a84877eaab
Amy Burns <amy.burns87@gmail.com>
2017-06-12 13:18:36 -0400

    Adding correct recipe re. xamarin/documentation#1262

---
+++ ios/media/images/rotate_an_image/RotateAnImage/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using UIKit;
+using CoreGraphics;
+
+namespace RotateAnImage
+{
+    public partial class ViewController : UIViewController
+    {
+		UIImage image;
+		UIImageView imageView;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            image = UIImage.FromBundle("dino-sml.jpeg");
+            imageView = new UIImageView(new CGRect(50, 50, 100, 100))
+            {
+                ContentMode = UIViewContentMode.ScaleAspectFit,
+                Image = image,
+                Transform = CGAffineTransform.MakeRotation((float)Math.PI / 4)                   
+            };
+
+            View.AddSubview(imageView);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+    }
+}
