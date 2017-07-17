2fdb6eb5bd8f381c39b7a71a186c4beefe763d3e
Tom Opgenorth <tom@opgenorth.net>
2017-07-17 10:45:18 -0600

    Update the recipe. See docs issue #2378.
    commit 5d00ded6959749993a2eda4f8d835f7d23cf7c63
    Author: Tom Opgenorth <tom@opgenorth.net>
    Date:   Mon Jul 17 10:42:04 2017 -0600
    
        Fix typo
    
    commit 6410cb59fb9a0c09dfa5fedeb42cd69f1174ae0c
    Author: Tom Opgenorth <tom@opgenorth.net>
    Date:   Mon Jul 17 09:14:22 2017 -0600
    
        Expand the sample for API21 and lower devices.
    
    commit dc2424420c0070c1b40b88520b6153506929dbe4
    Author: Tom Opgenorth <tom@opgenorth.net>
    Date:   Fri Jul 14 15:20:36 2017 -0600
    
        Add some more image buttons and some SVG files.
    
    commit caf4d1509a4f690aabd887a7abe20fdb3e36a3de
    Author: Tom Opgenorth <tom@opgenorth.net>
    Date:   Fri Jul 14 12:00:46 2017 -0600
    
        Update layout/look.

--- android/controls/imagebutton/create_an_imagebutton/ImageButtonDemo/Activity1.cs
+++ android/controls/imagebutton/create_an_imagebutton/ImageButtonDemo/Activity1.cs
@@ -1,30 +1,43 @@
 using System;
 
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V4.Content;
 
 namespace ImageButtonDemo
 {
-    [Activity (Label = "ImageButtonDemo", MainLauncher = true)]
-    public class Activity1 : Activity
+    [Activity (Label = "ImageButtonDemo", MainLauncher = true, Theme = "@style/Theme.MyTheme")]
+    public class Activity1 : Android.Support.V7.App.AppCompatActivity
     {
-        
-        protected override void OnCreate (Bundle bundle)
+        protected override void OnCreate (Bundle savedInstanceState)
         {
-            base.OnCreate (bundle);
-
-            // Set our view from the "main" layout resource
+            base.OnCreate (savedInstanceState);
             SetContentView (Resource.Layout.Main);
 
-            var button = FindViewById<ImageButton> (Resource.Id.myButton);
-            
-            button.Click += delegate {
-                Console.WriteLine ("button clicked");
-            };
+            //AppCompatDelegate.CompatVectorFromResourcesEnabled = true;
+            DisplayImageInButton();
+        }
+
+        void DisplayImageInButton() 
+        {
+            var button = FindViewById<ImageButton>(Resource.Id.myButton3);
+			var tv = FindViewById<TextView>(Resource.Id.dynamic_load_textview);
+			
+            if (Build.VERSION.SdkInt >=  BuildVersionCodes.Lollipop ) 
+            {
+                // Android 5.0 and higher can load the SVG image from resources.
+                var image = ContextCompat.GetDrawable(ApplicationContext, Resource.Drawable.ic_mood_black);
+				button.SetImageDrawable(image);
+			}
+            else 
+            {
+                // Devices running < Android 5.0 should use the VectorDrawableCompat
+                var image = Android.Support.Graphics.Drawable.VectorDrawableCompat.Create(this.Resources, Resource.Drawable.ic_sentiment_neutral_black_24dp, null);
+				button.SetImageDrawable(image);
+			}
+
         }
     }
 }
