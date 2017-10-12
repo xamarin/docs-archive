6e9585e0388e2f663c852dca1d438177a9ce3404
Mike Norman <mike.norman@xamarin.com>
2017-10-12 11:45:19 -0500

    AnimateUIView: Formatting cleanup

--- ios/animation/coreanimation/animate_a_uiview_using_uikit/AnimateUIView/AnimationViewController.cs
+++ ios/animation/coreanimation/animate_a_uiview_using_uikit/AnimateUIView/AnimationViewController.cs
@@ -19,34 +19,30 @@ namespace AnimateUIView
         {
             base.ViewDidLoad();
 
+            imageView = new UIImageView(new CGRect(0, 100, 50, 50));
+            image = UIImage.FromFile("Sample.png");
+            imageView.Image = image;
+            imageView.Alpha = 0.25f;
+            View.AddSubview(imageView);
+
             Action setCenterRight = () =>
             {
                 var xpos = UIScreen.MainScreen.Bounds.Right - imageView.Frame.Width / 2;
                 var ypos = imageView.Center.Y;
-
                 imageView.Center = new CGPoint(xpos, ypos);
             };
 
             Action setCenterLeft = () =>
             {
-                
                var xpos = UIScreen.MainScreen.Bounds.Left + imageView.Frame.Width / 2;
                var ypos = imageView.Center.Y;
                imageView.Center = new CGPoint(xpos, ypos);
-                   
             };
 
             Action setOpacity = () =>
             {
                 imageView.Alpha = 1;
             };
-
-			imageView = new UIImageView(new CGRect(0, 100, 50, 50));
-			image = UIImage.FromFile("Sample.png");
-			imageView.Image = image;
-            setCenterLeft();
-            imageView.Alpha = 0.25f;
-			View.AddSubview(imageView);
 
             UIViewPropertyAnimator propertyAnimator = new UIViewPropertyAnimator(4, UIViewAnimationCurve.EaseInOut, setCenterRight);
             propertyAnimator.AddAnimations(setOpacity);
