00a33e676f5ffd3ea4501dc75d934791e5fd3f23
David Britch <davidbritch@Davids-MBP.lan>
2017-11-15 10:35:20 +0000

    Removed deprecated Device APIs.

--- cross-platform/xamarin-forms/iOS/NSUserActivitySearch/NSUserActivitySearch/Views/TodoItemPage.xaml.cs
+++ cross-platform/xamarin-forms/iOS/NSUserActivitySearch/NSUserActivitySearch/Views/TodoItemPage.xaml.cs
@@ -3,68 +3,74 @@ using Xamarin.Forms;
 
 namespace NSUserActivitySearch
 {
-	public partial class TodoItemPage : ContentPage
-	{
-		IUserActivity userActivity;
-		bool isNewItem;
+    public partial class TodoItemPage : ContentPage
+    {
+        IUserActivity userActivity;
+        bool isNewItem;
 
-		public TodoItemPage (bool isNew = false)
-		{
-			InitializeComponent ();
-			isNewItem = isNew;
+        public TodoItemPage(bool isNew = false)
+        {
+            InitializeComponent();
+            isNewItem = isNew;
 
-			if (Device.OS == TargetPlatform.iOS) {
-				userActivity = DependencyService.Get<IUserActivity> ();
-			}
-		}
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                userActivity = DependencyService.Get<IUserActivity>();
+            }
+        }
 
-		protected override void OnAppearing ()
-		{
-			base.OnAppearing ();
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
 
-			if (Device.OS == TargetPlatform.iOS) {
-				var todoItem = (TodoItem)BindingContext;
-				userActivity.Start (todoItem);
-			}
-		}
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                var todoItem = (TodoItem)BindingContext;
+                userActivity.Start(todoItem);
+            }
+        }
 
-		protected override void OnDisappearing ()
-		{
-			if (Device.OS == TargetPlatform.iOS) {
-				userActivity.Stop ();
-			}
+        protected override void OnDisappearing()
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                userActivity.Stop();
+            }
 
-			base.OnDisappearing ();
-		}
+            base.OnDisappearing();
+        }
 
-		async void OnSaveActivated (object sender, EventArgs e)
-		{
-			var todoItem = (TodoItem)BindingContext;
-			if (isNewItem) {
-				App.TodoManager.Insert (todoItem);
-			} else {
-				App.TodoManager.Update (todoItem);
-			}
-			await Navigation.PopAsync ();
-		}
+        async void OnSaveActivated(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            if (isNewItem)
+            {
+                App.TodoManager.Insert(todoItem);
+            }
+            else
+            {
+                App.TodoManager.Update(todoItem);
+            }
+            await Navigation.PopAsync();
+        }
 
-		async void OnDeleteActivated (object sender, EventArgs e)
-		{
-			var todoItem = (TodoItem)BindingContext;
-			App.TodoManager.Delete (todoItem.ID);
-			await Navigation.PopAsync ();
-		}
+        async void OnDeleteActivated(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            App.TodoManager.Delete(todoItem.ID);
+            await Navigation.PopAsync();
+        }
 
-		async void OnCancelActivated (object sender, EventArgs e)
-		{
-			await Navigation.PopAsync ();
-		}
+        async void OnCancelActivated(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
 
-		void OnSpeakActivated (object sender, EventArgs e)
-		{
-			var todoItem = (TodoItem)BindingContext;
-			App.Speech.Speak (todoItem.Name + " " + todoItem.Notes);
-		}
-	}
+        void OnSpeakActivated(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            App.Speech.Speak(todoItem.Name + " " + todoItem.Notes);
+        }
+    }
 }
 
