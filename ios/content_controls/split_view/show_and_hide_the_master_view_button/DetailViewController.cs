99773624acf33207cb0ba4974b2be7c52c89171e
Mike Norman <mike.norman@xamarin.com>
2017-12-08 16:04:09 -0600

    Recipes: Show and Hide the Master View Button updates

--- ios/content_controls/split_view/show_and_hide_the_master_view_button/DetailViewController.cs
+++ ios/content_controls/split_view/show_and_hide_the_master_view_button/DetailViewController.cs
@@ -9,18 +9,34 @@ namespace SplitView
 		UILabel label;
 		UIToolbar toolbar;
 
-		public DetailViewController () : base()
-		{
-			View.BackgroundColor = UIColor.White;
-
-			label = new UILabel(new CGRect(100,70,300,50));
-			label.Text = "This is the detail view";
-			View.AddSubview (label);
-			// add a toolbar to host the master view popover (when it is required, in portrait)
-			toolbar = new UIToolbar(new CGRect(0, 40, View.Frame.Width, 30));
-			toolbar.AutoresizingMask = UIViewAutoresizing.FlexibleWidth;
-			View.AddSubview(toolbar);
-		}
+        public DetailViewController() : base()
+        {
+            View.BackgroundColor = UIColor.White;
+            // add a toolbar to host the master view popover (when it is required, in portrait)
+            toolbar = new UIToolbar();
+            toolbar.TranslatesAutoresizingMaskIntoConstraints = false;
+            var tbConstraints = new[]
+            {
+                  toolbar.LeadingAnchor.ConstraintEqualTo(this.View.SafeAreaLayoutGuide.LeadingAnchor),
+                  toolbar.TrailingAnchor.ConstraintEqualTo(this.View.SafeAreaLayoutGuide.TrailingAnchor),
+                  toolbar.TopAnchor.ConstraintEqualTo(this.View.SafeAreaLayoutGuide.TopAnchor, 0.0f),
+                  toolbar.HeightAnchor.ConstraintEqualTo(toolbar.IntrinsicContentSize.Height)
+            };
+            View.AddSubview(toolbar);
+            NSLayoutConstraint.ActivateConstraints(tbConstraints);
+            label = new UILabel();
+            label.Text = "This is the detail view";
+            label.TranslatesAutoresizingMaskIntoConstraints = false;
+            var lblConstraints = new[]
+            {
+                label.LeadingAnchor.ConstraintEqualTo(this.View.SafeAreaLayoutGuide.LeadingAnchor, 20.0f),
+                label.WidthAnchor.ConstraintEqualTo(label.IntrinsicContentSize.Width),
+                label.TopAnchor.ConstraintEqualTo(this.toolbar.BottomAnchor, 20.0f),
+                label.HeightAnchor.ConstraintEqualTo(label.IntrinsicContentSize.Height)
+            };
+            View.AddSubview(label);
+            NSLayoutConstraint.ActivateConstraints(lblConstraints);
+        }
 		
 		/// <summary>
 		/// Shows the button that allows access to the master view popover
