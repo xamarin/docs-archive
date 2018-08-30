b4e53f70e58c904cc6511d37566bc2acaa7a63f9
Tom Opgenorth <tom@opgenorth.net>
2018-08-30 08:24:59 -0600

    Freshen up the Android AutoCompleteTextView example.

--- Recipes/android/controls/autocomplete_text_view/add_an_autocomplete_text_input/Activity1.cs
+++ Recipes/android/controls/autocomplete_text_view/add_an_autocomplete_text_input/Activity1.cs
@@ -4,39 +4,56 @@ using Android.OS;
 using Android.Widget;
 using System.IO;
 using System.Collections.Generic;
+using Android.Support.V7.App;
 
 namespace AutoCompleteTextViewExample {
     [Activity(Label = "AutoCompleteTextView", MainLauncher = true, Icon = "@drawable/icon")]
-    public class Activity1 : Activity {
+    public class Activity1 : AppCompatActivity {
         protected override void OnCreate (Bundle bundle)
         {
             base.OnCreate (bundle);
 
             SetContentView(Resource.Layout.Main);
 
-            var autoCompleteOptions = new String[] { "Hello", "Hey", "Hej", "Hi", "Hola", "Bonjour", "Gday", "Goodbye", "Sayonara", "Farewell", "Adios" };
-            ArrayAdapter autoCompleteAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleDropDownItem1Line
-                , autoCompleteOptions);
+            var wordList = GetFewAutoCompleteOptions();
+
+            ArrayAdapter autoCompleteAdapter = new ArrayAdapter(this, 
+                Android.Resource.Layout.SimpleDropDownItem1Line, 
+                wordList);
 
             var autocompleteTextView = FindViewById<AutoCompleteTextView>(Resource.Id.AutoCompleteInput);
             autocompleteTextView.Adapter = autoCompleteAdapter;
 
-            #region Additional Information - use a file to populate autocomplete array
-            /* 
-            // instead of the small array of greetings, use a large dictionary of words loaded from a file
+        }
+
+        /// <summary>
+        /// This returns a small list of words for the Autocomplete TextView
+        /// </summary>
+        /// <returns>The few auto complete options.</returns>
+        String[] GetFewAutoCompleteOptions() 
+        {
+            return new String[] { "Hello", "Hey", "Hej", "Hi", "Hola", "Bonjour", "Gday", "Goodbye", "Sayonara", "Farewell", "Adios" };
+        }
+
+
+        /// <summary>
+        /// Retrieves a list of words from a text file.
+        /// </summary>
+        /// <returns>The many auto complete options.</returns>
+        String[] GetManyAutoCompleteOptions()
+        {
             Stream seedDataStream = Assets.Open(@"WordList.txt");
             List<string> lines = new List<string>();
-            using (StreamReader reader = new StreamReader(seedDataStream)) {
+            using (StreamReader reader = new StreamReader(seedDataStream))
+            {
                 string line;
-                while ((line = reader.ReadLine()) != null) {
+                while ((line = reader.ReadLine()) != null)
+                {
                     lines.Add(line);
                 }
             }
             string[] wordlist = lines.ToArray();
-            ArrayAdapter dictionaryAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleDropDownItem1Line, wordlist);
-            autocompleteTextView.Adapter = dictionaryAdapter;
-            */
-            #endregion
+            return wordlist;
         }
     }
 }
